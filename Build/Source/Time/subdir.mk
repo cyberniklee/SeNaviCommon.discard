619551be885502a65765dc4481d122883ea0104f
cyberniklee <cyberniklee@gmail.com>
2017-04-26 13:02:56 +0800

    p

--- Build/Source/Time/subdir.mk
+++ Build/Source/Time/subdir.mk
@@ -26,7 +26,7 @@ CPP_DEPS += \
 Source/Time/%.o: ../Source/Time/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: Cross G++ Compiler'
-	$(TARGET_CXX) -O0 -g3 -Wall -c -fmessage-length=0 -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	g++ -O0 -g3 -Wall -c -fmessage-length=0 -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
